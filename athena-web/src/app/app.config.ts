0d95e75c0dd50ae16eb4d58eb78416db9d7fe4b7
nooele2 <noellehtoo@gmail.com>
2025-11-19 15:46:51 +0700

    changes

--- athena-web/src/app/app.config.ts
+++ athena-web/src/app/app.config.ts
@@ -1,13 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withHashLocation()),
-    provideClientHydration()
+    provideRouter(routes, withHashLocation())
+    // Removed provideClientHydration() - not needed for GitHub Pages
   ]
 };
